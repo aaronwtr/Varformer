646d2775d3625720b39f925c5d463892d6b86197
Aaron Wenteler <aaronwenteler@gmail.com>
2023-02-08 17:04:47 +0000

    initialized implementation of tractability feature extractor

---
+++ src/model.py
@@ -0,0 +1,3 @@
+# TODO: Built XGBoost model with tractability features. Note that we have three target variables, whether the target has
+#  has an associated drug that is FDA approved for the small molecule, antibodies or protacs respectively. Think about
+#  how to handle this in the model.
