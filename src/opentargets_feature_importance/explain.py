7c242036467ae3cd5321ac8b5f27ea6dce42e0b6
Aaron Wenteler <aaronwenteler@gmail.com>
2023-02-15 15:30:46 +0000

    add F1 scoring metric and remove clinical trial features

--- src/opentargets_feature_importance/explain.py
+++ src/opentargets_feature_importance/explain.py
@@ -18,10 +18,11 @@ def make_model(X, y, model_type):
     model = XGBoostClassifier(scale_pos_weight=scale_pos_weight, model_type=model_type, num_boost_round=100)
     booster = model.fit(X_train, y_train)
 
-    accuracy, recall, confusion_matrix = model.score(X_test, y_test)
+    accuracy, recall, F1, confusion_matrix = model.score(X_test, y_test)
     print(f'{model_type} model results:')
     print("Accuracy: {:.2f}%".format(accuracy * 100))
     print("Recall: {:.2f}%".format(recall * 100))
+    print("F1 score: {:.2f}%\n".format(F1 * 100))
 
     return model, booster, X_test
 
@@ -34,13 +35,13 @@ def shap_explainer(model, test_data):
 
 if __name__ == '__main__':
     df_sm = GeneCharacterisation().tract_features[0]
-    X_sm = df_sm.iloc[:, 2:]
+    X_sm = df_sm.iloc[:, 4:]
     y_sm = df_sm.iloc[:, 1]
     model_sm, booster_sm, X_test_sm = make_model(X_sm, y_sm, 'Small Molecule')
     shap_explainer(booster_sm, X_test_sm)
 
     df_ab = GeneCharacterisation().tract_features[1]
-    X_ab = df_ab.iloc[:, 2:]
+    X_ab = df_ab.iloc[:, 4:]
     y_ab = df_ab.iloc[:, 1]
     model_ab, booster_ab, X_test_ab = make_model(X_ab, y_ab, 'Antibody')
     shap_explainer(booster_ab, X_test_ab)
