02d8104def8f14f4c7cfa2b339f2ae4e3e840ace
Aaron <aaronwenteler@gmail.com>
2023-11-15 01:44:13 +0000

    implemented lightning traininer

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 import yaml
 from sklearn.model_selection import train_test_split
+import lightning as pl
 
 from preprocessing import GeneCharacterisationPreprocessor
 from dataloader import DrugTargetData
+from model import PyTorchMLP, LightningMLP
 from torch.utils.data import DataLoader
-
 
 
 def main():
@@ -15,14 +16,41 @@ def main():
     print("Gene characterisation features preprocessed!\n")
 
     data = gcp.data
+    features = data.iloc[:, 1:-1].values
+    labels = data.iloc[:, -1].values
+    num_features = features.shape[1]
+    num_classes = len(set(labels))
+
     train, test = train_test_split(data, test_size=0.2, random_state=42)
-    train = DataLoader(DrugTargetData(data=train))
-    test = DataLoader(DrugTargetData(data=test))
 
-    # TODO check distribution of labels in train and test
+    train = DataLoader(
+        DrugTargetData(data=train),
+        batch_size=int(config['mlp']['batch_size']),
+        shuffle=True
+        )
 
+    test = DataLoader(DrugTargetData(data=test),
+                      batch_size=int(config['mlp']['batch_size']),
+                      shuffle=False
+                      )
 
-    print("Data loaded!\n")
+    mlp_pytorch = PyTorchMLP(config=config, num_features=num_features, num_classes=num_classes)
+    mlp_lightning = LightningMLP(model=mlp_pytorch, config=config)
+
+    trainer = pl.Trainer(
+        max_epochs=int(config['mlp']['epochs']),
+        accelerator='cpu'
+        )
+
+    trainer.fit(mlp_lightning, train, test)
+
+    # TODO:
+    #  Add metrics and logging of metrics
+    #  Plot training and validation loss
+    #  Add early stopping
+    #  Come up with validation strategy (ACMG gene set)
+    #  Hyperparameter tuning
+    #  Optional: debug mps accelerator (GPU)
 
 
 if __name__ == "__main__":
