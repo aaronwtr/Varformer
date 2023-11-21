90ebffb7b3344304a4d62c26597260d7657083d1
Aaron <aaronwenteler@gmail.com>
2023-11-21 18:42:16 +0000

    calculate labels and add evaluation metrics

--- src/model.py
+++ src/model.py
@@ -1,6 +1,7 @@
 import lightning as pl
 import torch
 import torch.nn.functional as F
+from torchmetrics import Accuracy, AUROC, SpearmanCorrCoef
 
 
 class PyTorchMLP(torch.nn.Module):
@@ -18,12 +19,23 @@ class PyTorchMLP(torch.nn.Module):
             torch.nn.ReLU(),
 
             # output layer
-            torch.nn.Linear(int(self.config['width_1']), num_classes)
+            torch.nn.Linear(int(self.config['width_2']), 1),
+            torch.nn.Sigmoid()
         )
 
+        self.train_acc = Accuracy(task="binary")
+        self.val_acc = Accuracy(task="binary")
+        self.train_auroc = AUROC(task="binary")
+        self.val_auroc = AUROC(task="binary")
+        self.train_spearman = SpearmanCorrCoef()
+        self.val_spearman = SpearmanCorrCoef()
+
     def forward(self, x):
-        logits = self.layers(x)
-        return logits
+        logits = self.layers(x).squeeze()
+        sigmoid = torch.nn.Sigmoid()
+        probabilities = sigmoid(logits)
+        binary_predictions = (probabilities > float(self.config['threshold'])).float()
+        return logits, probabilities, binary_predictions
 
 
 class LightningMLP(pl.LightningModule):
@@ -37,16 +49,22 @@ class LightningMLP(pl.LightningModule):
 
     def training_step(self, batch, batch_idx):
         features, labels = batch
-        logits = self(features)
-        loss = F.cross_entropy(logits, labels)
+        logits, probas, bin_preds = self(features)
+        loss = F.binary_cross_entropy_with_logits(logits, labels.float())
         self.log('train_loss', loss)
+        self.log('train_acc', self.model.train_acc(bin_preds, labels))
+        self.log('train_auroc', self.model.train_auroc(bin_preds, labels))
+        self.log('train_spearman', self.model.train_spearman(probas, labels.float()))
         return loss
 
     def validation_step(self, batch, batch_idx):
         features, labels = batch
-        logits = self(features)
-        loss = F.cross_entropy(logits, labels)
+        logits, probas, bin_preds = self(features)
+        loss = F.binary_cross_entropy_with_logits(logits, labels.float())
         self.log('val_loss', loss)
+        self.log('val_acc', self.model.train_acc(bin_preds, labels))
+        self.log('val_auroc', self.model.train_auroc(bin_preds, labels))
+        self.log('val_spearman', self.model.train_spearman(probas, labels.float()))
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=float(self.config['lr']))
