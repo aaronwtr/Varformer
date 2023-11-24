31f5d7248f8efb7e8143833d8c08e2f839d5d1ff
Aaron <aaronwenteler@gmail.com>
2023-11-24 18:44:48 +0000

    improved tuning

--- src/main.py
+++ src/main.py
@@ -1,18 +1,44 @@
-import os
-
 import yaml
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-import lightning as pl
-from pytorch_lightning.loggers import WandbLogger
+import os
+import torch
+import optuna
 
+import lightning as pl
+
+from pytorch_lightning.loggers import WandbLogger
 from preprocessing import GeneCharacterisationPreprocessor, MissenseVariantPreprocessor
 from dataloader import DrugTargetData
 from model import PyTorchMLP, LightningMLP
 from torch.utils.data import DataLoader
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
 
-def main():
+def tuning(trial: optuna.trial.Trial) -> float:
+    with open("config.yml", 'r') as stream:
+        config = yaml.safe_load(stream)
+
+    config = config['hyperparameters']
+
+    config['mlp']['width_1'] = trial.suggest_categorical('width_1', [2, 6, 8, 16, 32, 64, 128])
+    config['mlp']['lr'] = trial.suggest_categorical('lr', [1e-2, 1e-3, 1e-4, 1e-5])
+    config['mlp']['batch_size'] = trial.suggest_categorical('batch_size', [64, 128, 256, 512, 1024])
+    config['mlp']['optimizer'] = trial.suggest_categorical('optimizer', ['Adam', 'AdamW', 'RMSprop',
+                                                                         'SGD'])
+
+    trainer = training(tag="Tuning")
+
+    hyperparameters = dict(
+        width_1=config['mlp']['width_1'],
+        lr=config['mlp']['lr']
+    )
+
+    trainer.logger.log_hyperparams(hyperparameters)
+
+    return trainer.callback_metrics["val_spearman"].item()
+
+
+def training(tag="Training"):
     with open("config.yml", 'r') as stream:
         config = yaml.safe_load(stream)
 
@@ -21,6 +47,8 @@ def main():
 
     gcp = GeneCharacterisationPreprocessor(config=config)
     print("Gene characterisation features preprocessed!\n")
+
+    config = config['hyperparameters']
 
     data = gcp.data
     features = data.iloc[:, 1:-1].values
@@ -51,29 +79,78 @@ def main():
         shuffle=False
         )
 
-    train_imbalance = 1 / float(train.dataset.label_imbalance().item())
+    train_imbalance = 1 / float(train.dataset.label_imbalance().item())     # calculate inverse class frequency
 
     mlp_pytorch = PyTorchMLP(config=config, num_features=num_features)
-    mlp_lightning = LightningMLP(model=mlp_pytorch, config=config)
+    mlp_lightning = LightningMLP(model=mlp_pytorch, config=config, imbalance=train_imbalance)
 
-    wandb_logger = WandbLogger(log_model="all")
+    if torch.cuda.is_available():
+        accelerator = 'gpu'
+    else:
+        accelerator = 'cpu'
 
-    trainer = pl.Trainer(
-        max_epochs=int(config['mlp']['epochs']),
-        accelerator='cpu',
-        enable_progress_bar=False,
-        logger=wandb_logger
+    if tag == "Training":
+        wandb_logger = WandbLogger(
+            project="drug-target-prediction",
+            tags=["mlp", {tag}],
+            log_model="all"
         )
+
+        trainer = pl.Trainer(
+            max_epochs=int(config['mlp']['epochs']),
+            accelerator=accelerator,
+            enable_progress_bar=False,
+            logger=wandb_logger
+        )
+    elif tag == "Tuning":
+        trainer = pl.Trainer(
+            max_epochs=int(config['mlp']['epochs']),
+            accelerator=accelerator,
+            enable_progress_bar=False
+            )
+    else:
+        raise ValueError("Invalid tag. Pick from 'Training' or 'Tuning'")
 
     trainer.fit(mlp_lightning, train, test)
 
+    return trainer
+
     # TODO:
-    #  - Normalize features after train, test, splitting
-    #  - Add early stopping
-    #  - Come up with validation strategy (ACMG gene set)
     #  - Hyperparameter tuning
+    #  - Scale up to find loss convergence epoch
+    #  - Set up checkpointing to save the model with the best validation auroc
+    #  - Come up with validation strategy (ACMG gene set)
 
 
 if __name__ == "__main__":
-    main()
+    files = os.listdir()
+    study = optuna.create_study(
+        study_name="gdtp_mlp",
+        direction="maximize"
+    )
+    n_trials = 3
+
+    study.optimize(tuning, n_trials=n_trials, show_progress_bar=True)
+
+    print("Number of finished trials: {}".format(len(study.trials)))
+
+    print("Best trial, optimized for Spearman:")
+    best_trial = study.best_trial
+
+    print("  Metric: {}".format(best_trial.value))
+
+    print("  Params: ")
+    for key, value in best_trial.params.items():
+        print("    {}: {}".format(key, value))
+
+    with open("best_hyperparameters.txt", "a") as f:
+        if os.stat("best_hyperparameters.txt").st_size == 0:
+            f.write("Run ID\tSpearman Corr.\tHyperparameters\n")
+            run_id = 0
+        else:
+            with open("best_hyperparameters.txt", "r") as f:
+                lines = f.readlines()
+                last_line = lines[-1]
+                run_id = int(last_line.split("\t")[0]) + 1
+        f.write(f"{run_id}\t{best_trial.value}\t{best_trial.params}\n")
     
