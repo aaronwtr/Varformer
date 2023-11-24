2ec0a0ad4c2b692fea47e5523a53d6a8d995267c
Aaron <aaronwenteler@gmail.com>
2023-11-24 18:44:58 +0000

    weight initialization

--- src/model.py
+++ src/model.py
@@ -1,6 +1,8 @@
-import lightning as pl
 import torch
+
+import lightning as pl
 import torch.nn.functional as F
+
 from torchmetrics import Accuracy, AUROC, SpearmanCorrCoef
 
 
@@ -8,7 +10,6 @@ class PyTorchMLP(torch.nn.Module):
     def __init__(self, config, num_features):
         super().__init__()
         self.config = config['mlp']
-
         self.layers = torch.nn.Sequential(
             # input layer
             torch.nn.Linear(num_features, int(self.config['width_1'])),
@@ -26,6 +27,12 @@ class PyTorchMLP(torch.nn.Module):
             # torch.nn.Sigmoid()
         )
 
+        for m in self.modules():
+            if isinstance(m, torch.nn.Linear):
+                torch.nn.init.xavier_uniform_(m.weight)
+                if m.bias is not None:
+                    torch.nn.init.zeros_(m.bias)
+
         self.train_acc = Accuracy(task="binary")
         self.val_acc = Accuracy(task="binary")
         self.train_auroc = AUROC(task="binary")
@@ -42,9 +49,10 @@ class PyTorchMLP(torch.nn.Module):
 
 
 class LightningMLP(pl.LightningModule):
-    def __init__(self, model, config):
+    def __init__(self, model, config, imbalance):
         super().__init__()
         self.model = model
+        self.imbalance = imbalance
         self.config = config['mlp']
 
     def forward(self, x):
@@ -53,7 +61,8 @@ class LightningMLP(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         features, labels = batch
         logits, probas, bin_preds = self(features)
-        loss = F.binary_cross_entropy_with_logits(logits, labels.float())
+        class_weight = torch.tensor([1 if labels[i] == 0 else self.imbalance for i in range(len(labels))])
+        loss = F.binary_cross_entropy_with_logits(logits, labels.float(), weight=class_weight)
         self.log('train_loss', loss)
         self.log('train_acc', self.model.train_acc(bin_preds, labels))
         self.log('train_auroc', self.model.train_auroc(bin_preds, labels))
@@ -63,12 +72,21 @@ class LightningMLP(pl.LightningModule):
     def validation_step(self, batch, batch_idx):
         features, labels = batch
         logits, probas, bin_preds = self(features)
-        loss = F.binary_cross_entropy_with_logits(logits, labels.float(), pos_weight=torch.tensor([1, 20]))
+        loss = F.binary_cross_entropy_with_logits(logits, labels.float())
         self.log('val_loss', loss)
         self.log('val_acc', self.model.train_acc(bin_preds, labels))
         self.log('val_auroc', self.model.train_auroc(bin_preds, labels))
         self.log('val_spearman', self.model.train_spearman(probas, labels.float()))
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=float(self.config['lr']))
+        if self.config['optimizer'] == "Adam":
+            optimizer = torch.optim.Adam(self.parameters(), lr=float(self.config['lr']))
+        elif self.config['optimizer'] == "SGD":
+            optimizer = torch.optim.SGD(self.parameters(), lr=float(self.config['lr']))
+        elif self.config['optimizer'] == "RMSprop":
+            optimizer = torch.optim.RMSprop(self.parameters(), lr=float(self.config['lr']))
+        elif self.config['optimizer'] == "AdamW":
+            optimizer = torch.optim.AdamW(self.parameters(), lr=float(self.config['lr']))
+        else:
+            raise ValueError(f"Optimizer {self.config['optimizer']} not recognized.")
         return optimizer
