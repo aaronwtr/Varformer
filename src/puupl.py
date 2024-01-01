16006865bff5e95f4e3bed093acd0c75c5abf46a
Aaron <aaronwenteler@gmail.com>
2024-01-01 18:27:23 +0100

    deprecate puupl

--- src/puupl.py
+++ src/puupl.py
@@ -1,3 +1,9 @@
+"""
+
+PUUPL has been deprecated due to instability in predicting the pseudo labels with untrained models.
+
+"""
+
 import torch
 import random
 
@@ -14,16 +20,16 @@ from matplotlib import pyplot as plt
 def training(train, val, config):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    P = torch.tensor([i for i, labels in enumerate(train.dataset.labels) if labels == 1]).to(device)
-    U = torch.tensor([i for i, labels in enumerate(train.dataset.labels) if labels == 0]).to(device)
-    L = torch.tensor([]).to(device)
+    P = torch.tensor([i for i, labels in enumerate(train.dataset.labels) if labels == 1])
+    U = torch.tensor([i for i, labels in enumerate(train.dataset.labels) if labels == 0])
+    L = torch.tensor([])
 
     K = 2
     T = 1000
     t_l = 0.05
     t_u = 0.35
 
-    models = [m.PyTorchMLP(config=config, num_features=train.dataset.data.shape[1], model_type="puupl").to(device) for _
+    models = [m.PyTorchMLP(config=config, num_features=train.dataset.data.shape[1], model_type="puupl") for _
               in
               range(K)]
 
@@ -40,55 +46,71 @@ def training(train, val, config):
 
     converged = False
     val_losses = []
-    pseudo_labels = []
+    pseudo_labels = {}
     metrics_data = {
         'train_loss_model1': [],
-        'val_loss_model1': [],
-        'train_auroc_model1': [],
-        'val_auroc_model1': [],
-        'train_precision_model1': [],
-        'val_precision_model1': [],
+        # 'val_loss_model1': [],
+        # 'train_auroc_model1': [],
+        # 'val_auroc_model1': [],
+        # 'train_precision_model1': [],
+        # 'val_precision_model1': [],
     }
     epoch = 1
     while not converged:
         print("Epoch: ", epoch)
         for i, model in enumerate(models):
             model.load_state_dict(weights[i])
-            train_loss, _, auroc, precision, _ = train_model(models[i], device, optimizers[i], train, criterion, P, U,
+            train_loss, _, auroc, precision, _ = train_model(models[i], optimizers[i], train, criterion, P, U,
                                                              L, pseudo_labels)
             print(f"Train loss model {i}: ", train_loss)
-            print(f"Train auroc model {i}: ", float(auroc))
-            print(f"Train precision model {i}: ", float(precision))
+            # print(f"Train auroc model {i}: ", float(auroc))
+            # print(f"Train precision model {i}: ", float(precision))
 
             if i == 0:
                 metrics_data['train_loss_model1'].append(float(train_loss))
-                metrics_data['train_auroc_model1'].append(float(auroc))
-                metrics_data['train_precision_model1'].append(float(precision))
+                # metrics_data['train_auroc_model1'].append(float(auroc))
+                # metrics_data['train_precision_model1'].append(float(precision))
 
         val_losses = update_ensemble_weights(models, val, criterion, val_losses, P, U, L, pseudo_labels)
-        metrics_data['val_loss_model1'].append(val_losses[-2])
-        metrics_data['val_auroc_model1'].append(val_losses[-2])
-        metrics_data['val_precision_model1'].append(val_losses[-2])
+        # metrics_data['val_loss_model1'].append(val_losses[-2])
+        # metrics_data['val_auroc_model1'].append(val_losses[-2])
+        # metrics_data['val_precision_model1'].append(val_losses[-2])
 
-        # if epoch % 10 == 0:
-        #     for metric_name, metric_values in metrics_data.items():
-        #         plt.figure(figsize=(10, 6))
-        #         moving_averages = moving_average(metric_values, window=10)
-        #         plt.plot(moving_averages)
-        #         plt.title(f'{metric_name} Moving Average')
-        #         plt.xlabel('Epoch')
-        #         plt.ylabel(metric_name)
-        #         plt.show()
+        if epoch % 5 == 0:
+            for metric_name, metric_values in metrics_data.items():
+                plt.figure(figsize=(10, 6))
+                moving_averages = moving_average(metric_values, window=5)
+                plt.plot(moving_averages)
+                plt.title(f'{metric_name} Moving Average')
+                plt.xlabel('Epoch')
+                plt.ylabel(metric_name)
+                plt.show()
 
         unlabelled_data = train.dataset.features[U]
-        new_labeled_examples, new_unlabeled_examples, pseudo_labels = pseudo_label(models, epoch, device,
-                                                                                   unlabelled_data, t_l, t_u, T)
+        L_new_pos, L_new_neg, U_new, pseudo_labels_pos_new, pseudo_labels_neg_new = pseudo_label(models, epoch,
+                                                                                                 unlabelled_data, P, U,
+                                                                                                 t_l, t_u, T)
 
-        L = torch.cat((L, new_labeled_examples), dim=0)
-        L = L.to(torch.int64)
-        U = torch.cat((U, new_unlabeled_examples), dim=0)
-        U = torch.tensor([i for i in U if i not in L])
-        U = U.to(torch.int64)
+        U_new = torch.cat((U, U_new), dim=0)
+        U = torch.tensor([i for i in U_new if i not in L])
+        U = U.clone().detach().to(torch.int64)
+
+        L_pos_list = L_new_pos.tolist()
+        L_pos_list = [int(i) for i in L_pos_list]
+        L_neg_list = L_new_neg.tolist()
+        L_neg_list = [int(i) for i in L_neg_list]
+        L_new_list = L_pos_list + L_neg_list
+
+        pseudo_labels_pos = pseudo_labels_pos_new.tolist()
+        pseudo_labels_pos = [float(i) for i in pseudo_labels_pos]
+        pseudo_labels_neg = pseudo_labels_neg_new.tolist()
+        pseudo_labels_neg = [float(i) for i in pseudo_labels_neg]
+        pseudo_labels_new = pseudo_labels_pos + pseudo_labels_neg
+
+        pseudo_labels_new = dict(zip(L_new_list, pseudo_labels_new))
+        pseudo_labels = {**pseudo_labels, **pseudo_labels_new}
+
+        L = torch.tensor([i for i in pseudo_labels.keys()])
 
         if has_converged(val_losses, threshold=0.001):
             # TODO: Check how this progresses
@@ -109,20 +131,23 @@ def batching_labels(data, batch_idx, P, U, L):
     P_batch = []
     U_batch = []
     L_batch = []
+    L_batch_reset = []
     for i in range(len(data)):
-        if i * (batch_idx + 1) in P:
+        if (batch_idx * len(data)) + i in P:
             P_batch.append(i)
-        elif i * (batch_idx + 1) in U:
+        elif (batch_idx * len(data)) + i in U:
             U_batch.append(i)
-        elif i * (batch_idx + 1) in L:
-            L_batch.append(i)
+        elif (batch_idx * len(data)) + i in L:
+            L_batch.append((batch_idx * len(data)) + i)
+            L_batch_reset.append(i)
     P_batch = torch.tensor(P_batch)
     U_batch = torch.tensor(U_batch)
     L_batch = torch.tensor(L_batch)
-    return P_batch, U_batch, L_batch
+    L_batch_reset = torch.tensor(L_batch_reset)
+    return P_batch, U_batch, L_batch, L_batch_reset
 
 
-def train_model(model, device, optimizer, train, criterion, P, U, L, pseudo_labels):
+def train_model(model, optimizer, train, criterion, P, U, L, pseudo_labels):
     model.train()
     total_loss = 0.0
     total_acc = 0.0
@@ -132,12 +157,17 @@ def train_model(model, device, optimizer, train, criterion, P, U, L, pseudo_labe
     num_batches = 0
 
     for batch_idx, (data, labels) in enumerate(train):
-        data, labels = data.to(device), labels.to(device)
-        P_batch, U_batch, L_batch = batching_labels(data, batch_idx, P, U, L)
+        # NOTE: for the normal labels (P and U), the batch indices are reset between 0 and batch_size every iteration
+        # while for the pseudo labels (L), the batch indices are carried over between iterations because we access the
+        # values from the pseudo_labels dictionary
+        data, labels = data, labels
+        P_batch, U_batch, L_batch, L_batch_reset = batching_labels(data, batch_idx, P, U, L)
+        pseudo_labels_tensor = torch.tensor([pseudo_labels[int(i.item())] for i in L_batch])
 
         logits, probas, bin_preds = model(data)
 
-        loss = criterion(logits, P=P_batch, U=U_batch, L=L_batch, pseudo_labels=pseudo_labels)
+        loss = criterion(logits, P=P_batch, U=U_batch, L=L_batch, L_reset=L_batch_reset, labels=labels,
+                         pseudo_labels=pseudo_labels_tensor)
 
         # Backward pass
         optimizer.zero_grad()
@@ -192,15 +222,15 @@ def has_converged(val_losses, threshold=0.001):
         return False
 
 
-def pseudo_label(models, epoch, device, X_U, t_l, t_u, T):
-    X_U = X_U.to(device)
-    logits = [model(X_U)[0] for model in models]
+def pseudo_label(models, epoch, X_U, P, U, t_l, t_u, T):
+    logits = [model(X_U)[0].clone().detach() for model in models]
     probs = [F.sigmoid(logit) for logit in logits]
 
     stacked = torch.stack(probs)
     probs_avg = torch.mean(stacked, dim=0)
-    # plot the distribution of probs_avg
 
+    # # plot the distribution of probs_avg
+    #
     probs_avg_np = probs_avg.detach().numpy()  # Convert tensor to numpy array
     fig, ax = plt.subplots(dpi=300)
     ax.hist(probs_avg_np, bins=50, edgecolor='black')
@@ -209,7 +239,7 @@ def pseudo_label(models, epoch, device, X_U, t_l, t_u, T):
     ax.set_ylabel('Frequency')
     plt.show()
 
-    max_avg = torch.max(probs_avg[0])     # Convert tensor to numpy array
+    max_avg = torch.max(probs_avg[0])  # Convert tensor to numpy array
 
     aleatoric = -1 / len(probs) * (torch.sum(stacked * torch.log(stacked), dim=0) +
                                    torch.sum((1 - stacked) * torch.log(1 - stacked), dim=0))
@@ -219,30 +249,37 @@ def pseudo_label(models, epoch, device, X_U, t_l, t_u, T):
     # Rank by epistemic uncertainty
     sorted_indices = torch.argsort(epistemic)
 
-    # Take most confident T examples
     confident_indices = sorted_indices[:T]
     L_new = [i for i in confident_indices if epistemic[i] <= t_l]
 
     # Balance positive/negative
-    L_new_pos = [i for i in L_new if probs_avg[i] > 0.17]
-    # L_new_neg = [i for i in L_new if probs_avg[i] <= 0.5]
-    L_new_neg = []
-    L_new = L_new_pos + L_new_neg
-    L_new = torch.tensor(L_new)
+    L_new_pos = [i for i in L_new if probs_avg[i] > 0.65 and i not in P]
+    L_new_neg = [i for i in L_new if probs_avg[i] <= 0.65 and i not in P]  # TODO: Link this to config threshold
+    L_new_neg = L_new_neg[:len(L_new_pos)]
 
     if len(L_new) == 0:
-        soft_labels = torch.tensor([])
+        soft_labels_pos = torch.tensor([])
+        soft_labels_neg = torch.tensor([])
     else:
-        soft_labels = probs_avg[L_new]
+        soft_labels_pos = probs_avg[L_new_pos]
+        soft_labels_neg = torch.zeros_like(soft_labels_pos)
 
     unreliable_indices = [i for i in L_new if epistemic[i] >= t_u]
-    U_new = torch.tensor(unreliable_indices)
+    U_unreliable = torch.tensor(unreliable_indices)
 
-    L_new = torch.tensor(L_new).to(device)
-    U_new = torch.tensor(U_new).to(device)
-    soft_labels = soft_labels.to(device)
+    if len(U_unreliable) == 0:
+        U_new = torch.tensor([])
+    else:
+        U_new = U[U_unreliable]
 
-    return L_new, U_new, soft_labels
+    L_new_pos = U[L_new_pos]
+    L_new_neg = U[L_new_neg]
+
+    U_new = U_new.clone().detach()
+    soft_labels_pos_new = soft_labels_pos.clone().detach()
+    soft_labels_neg_new = soft_labels_neg.clone().detach()
+
+    return L_new_pos, L_new_neg, U_new, soft_labels_pos_new, soft_labels_neg_new
 
 
 def update_ensemble_weights(models, val, criterion, val_losses, P, U, L, pseudo_labels):
@@ -272,10 +309,12 @@ def evaluate(model, val, criterion, P, U, L, pseudo_labels):
         num_batches = 0
 
         for batch_idx, (data, labels) in enumerate(val):
-            P_batch, U_batch, L_batch = batching_labels(data, batch_idx, P, U, L)
+            P_batch, U_batch, L_batch, L_batch_reset = batching_labels(data, batch_idx, P, U, L)
             logits, probas, bin_preds = model(data)
+            pseudo_labels_tensor = torch.tensor([pseudo_labels[int(i.item())] for i in L_batch])
 
-            loss = criterion(logits, P=P_batch, U=U_batch, L=L_batch, pseudo_labels=labels)
+            loss = criterion(logits, P=P_batch, U=U_batch, L=L_batch, L_reset=L_batch_reset, labels=labels,
+                             pseudo_labels=pseudo_labels_tensor)
 
             total_loss += loss.item()
             total_loss += loss.item()
