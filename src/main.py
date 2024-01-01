68db1bbd233a1ee3f1bf5cd3544348f8751d70a3
Aaron <aaronwenteler@gmail.com>
2024-01-01 18:27:00 +0100

    updating todo and add distillation

--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ def main(mode="training"):
         train.training(tag="Standard Training")
     elif mode == "puupl":
         train.training(tag="PUUPL Training")
+    elif mode == "distillation":
+        train.distillation()
     elif mode == "tuning":
         train.tuning()
     elif mode == "kfold_training":
@@ -15,14 +17,13 @@ def main(mode="training"):
 
 
 if __name__ == "__main__":
-    main(mode="puupl")
+    main(mode="distillation")
 
     # TODO:
     #  MLP model
     #  [X] Set up checkpointing to save the model with the best validation auroc
     #  [X] Make sure to save hyperparameters per model in wandb or locally
-    #  [X] Hyperparameter tuning
-    #  [X] Find out how many epochs to train for (500)
+    #  [X] Find out how many epochs to train for (100)
     #  [X] Add biological process and molecular function features from HPA (think about implications)
     #  [X] Add cellular target localization features from HPA
     #  [X] Check normalization after introduction of new features
@@ -32,9 +33,12 @@ if __name__ == "__main__":
     #  [X] Hold out golden standard 32 drug targets
     #  [X] Check overlap between golden standard and ACMG genes
     #  [X] Check hold out drug targets for common essential genes
-    #  [X] Setup PUL (https://arxiv.org/abs/2201.13192)
-    #  [ ] Hyperparameter tuning with PUL
-    #  [ ] Train baseline neural network model
+    #  [X] Train baseline neural network model
+    #  [FAILED] Setup PUUPL (To unstable to use, need a properly trained model to do this. --> Self-distillation)
+    #  [ ] Hyperparameter tuning run where we add logging of width/depth ratio and total number of parameters
+    #  [ ] Setup self-distillation
+    #  [ ] Self-distillation such that probas around the threshold are kept as unlabeled data and in this way
+    #      iteratively gather pseudolabels
     #  [ ] Train final models
     #  [ ] Evaluate on held out test set
     #  [ ] Add SHAP interpretability
