2098a46f38ca4427cc9a66361e7f721cca603ae2
Aaron <aaronwenteler@gmail.com>
2024-01-11 22:17:10 +0000

    VariantPathogenicityData class

--- src/dataloader.py
+++ src/dataloader.py
@@ -22,3 +22,19 @@ class DrugTargetData(Dataset):
 
     def label_imbalance(self):
         return self.labels.sum() / len(self.labels)
+
+
+class VariantPathogenicityData(Dataset):
+    def __init__(self, data_dict):
+        self.gene_names = list(data_dict.keys())
+        self.variant_pathogenicities = list(data_dict.values())
+
+        self.variant_pathogenicities = [torch.tensor(v, dtype=torch.float32) for v in self.variant_pathogenicities]
+
+    def __len__(self):
+        return len(self.gene_names)
+
+    def __getitem__(self, idx):
+        gene_name = self.gene_names[idx]
+        variant_pathogenicities = self.variant_pathogenicities[idx]
+        return gene_name, variant_pathogenicities
