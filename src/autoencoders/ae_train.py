99b6bed8250267f53e48e61826fae880dcf0f36a
Aaron <aaronwenteler@gmail.com>
2024-01-12 19:00:52 +0000

    setup lightning for autoencoder training

--- src/autoencoders/ae_train.py
+++ src/autoencoders/ae_train.py
@@ -3,23 +3,24 @@ from pytorch_lightning.loggers import WandbLogger
 from src.autoencoders.autoencoder import AutoencoderTrainer
 from src.dataloader import VariantPathogenicityData
 from torch.utils.data import DataLoader
-from sklearn.model_selection import train_test_split
+import torch.nn.functional as F
+from torch import nn
+from pytorch_lightning.callbacks import ModelCheckpoint
 
 import wandb
+import torch
 
 
 def train(data_dict, config):
-    # TODO:
-    #  - [X] setup training
-    #  - [ ] fix autoencoding with batch size > 1
-
     hparams = config['hyperparameters']['pathogenicity_autoencoder']
 
-    # wandb.init(project='danio-autoencoders')
+    wandb.init(project='danio-autoencoders')
 
     variant_pathogenicity = DataLoader(
-        VariantPathogenicityData(data_dict=data_dict),
+        VariantPathogenicityData(data_dict=data_dict, reduct_dim=hparams['input_dim'],
+                                 reduction_type=hparams['reduction']),
         batch_size=hparams['batch_size'],
+        collate_fn=padding,
         shuffle=True
     )
 
@@ -27,7 +28,42 @@ def train(data_dict, config):
                                encoding_dim=hparams['latent_dim'], num_layers=hparams['num_layers'],
                                nhead=hparams['nhead'], reduction_type=hparams['reduction'])
 
-    # wandb_logger = WandbLogger()
+    wandb_logger = WandbLogger()
 
-    trainer = Trainer(max_epochs=30)
+    run_name = wandb_logger.experiment.name
+    checkpoint_callback = ModelCheckpoint(
+        monitor='epoch',
+        dirpath='checkpoints',
+        filename=f'{run_name}' + '-{epoch:02d}-{val_auroc:.2f}',
+        save_top_k=1,
+        mode='min',
+    )
+
+    trainer = Trainer(max_epochs=hparams['max_epochs'], logger=wandb_logger, callbacks=[checkpoint_callback])
     trainer.fit(model, variant_pathogenicity)
+
+
+def padding(batch):
+    X = []
+    for x, reduct_dim in batch:
+        current_dimension = x.size(-1)
+        if current_dimension == reduct_dim:
+            X.append(x)
+        elif current_dimension < reduct_dim:
+            padding_left = (reduct_dim - current_dimension) // 2
+            padding_right = reduct_dim - current_dimension - padding_left
+            padded_x = F.pad(x, (padding_left, padding_right), value=0)
+            X.append(padded_x)
+        else:
+            pooled_x = pooling(x, reduct_dim)
+            pooled_x = pooled_x.squeeze(0)
+            X.append(pooled_x)
+    X = torch.stack(X)
+    return X
+
+
+def pooling(x, reduct_dim):
+    x = x.unsqueeze(0)
+    pool = nn.AdaptiveAvgPool1d(reduct_dim)
+    pooled_x = pool(x)
+    return pooled_x
