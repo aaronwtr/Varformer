8d7847e252fbe991ec69405a405b4bddec7af1d1
Aaron <aaronwenteler@gmail.com>
2024-01-12 19:01:34 +0000

    add reduction algorithm

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,5 +1,10 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
 from torch.utils.data import Dataset
+
+from src.autoencoders.autoencoder import AutoencoderTrainer
 
 
 class DrugTargetData(Dataset):
@@ -25,16 +30,55 @@ class DrugTargetData(Dataset):
 
 
 class VariantPathogenicityData(Dataset):
-    def __init__(self, data_dict):
+    def __init__(self, data_dict, reduct_dim, reduction_type="padding"):
         self.gene_names = list(data_dict.keys())
         self.variant_pathogenicities = list(data_dict.values())
 
         self.variant_pathogenicities = [torch.tensor(v, dtype=torch.float32) for v in self.variant_pathogenicities]
 
+        self.reduction_type = reduction_type
+        self.reduct_dim = reduct_dim
+
     def __len__(self):
         return len(self.gene_names)
 
     def __getitem__(self, idx):
-        gene_name = self.gene_names[idx]
         variant_pathogenicities = self.variant_pathogenicities[idx]
-        return gene_name, variant_pathogenicities
+        reduct_dim = self.reduct_dim
+        return variant_pathogenicities, reduct_dim
+
+    def reduction(self, x):
+        if self.reduction_type == "padding":
+            x = self.padding(x)
+        elif self.reduction_type == "pooling":
+            x = self.pooling(x)
+        else:
+            raise ValueError("Invalid reduction type. Expected 'padding' or 'pooling'.")
+        return x
+
+    def padding(self, x):
+        current_dimension = x.size(-1)
+        if current_dimension == self.reduct_dim:
+            return x
+        elif current_dimension < self.reduct_dim:
+            # Calculate the required padding on both sides
+            padding_left = (self.reduct_dim - current_dimension) // 2
+            padding_right = self.reduct_dim - current_dimension - padding_left
+
+            # Pad the tensor
+            padded_x = F.pad(x, (padding_left, padding_right), value=0)
+            return padded_x
+        else:
+            raise ValueError("Current dimension is already greater than the target dimension.")
+
+    def pooling(self, x):
+        current_dimension = x.size(-1)
+
+        if current_dimension == self.reduct_dim:
+            return x
+        elif current_dimension > self.reduct_dim:
+            pool = nn.AdaptiveAvgPool1d(self.reduct_dim)
+            pooled_x = pool(x)
+            return pooled_x
+        else:
+            raise ValueError("Current dimension is already smaller than the target dimension.")
