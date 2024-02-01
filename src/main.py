5088db99b9c6e261b0d0425476857e4c0f3c5f37
Aaron <aaronwenteler@gmail.com>
2024-02-01 17:27:25 +0000

    add testing mode

--- src/main.py
+++ src/main.py
@@ -1,46 +1,32 @@
-import train
+import training
+import testing
 
 
 def main(mode="training"):
     if mode == "training":
-        train.training(tag="Standard Training")
+        training.train(tag="Standard Training")
     elif mode == "puupl":
-        train.training(tag="PUUPL Training")
-    elif mode == "distillation":
-        train.distillation()
+        training.train(tag="PUUPL Training")
     elif mode == "tuning":
-        train.tuning()
-    elif mode == "kfold_training":
-        train.kfold()
+        training.tune()
+    elif mode == "kfold_teacher":
+        training.kfold_teacher()
+    elif mode == "kfold_student":
+        training.kfold_student()
+    elif mode == "testing":
+        testing.test()
     else:
-        raise ValueError("Invalid mode. Pick from 'training' or 'tuning'")
+        raise ValueError("Invalid mode. Pick from 'training' or 'tuning', 'kfold_student',"
+                         "'kfold_teacher, 'testing', or 'puupl'")
 
 
 if __name__ == "__main__":
-    main(mode="distillation")
+    main(mode="kfold_student")
 
     # TODO:
     #  MLP model
-    #  [X] Set up checkpointing to save the model with the best validation auroc
-    #  [X] Make sure to save hyperparameters per model in wandb or locally
-    #  [X] Find out how many epochs to train for (100)
-    #  [X] Add biological process and molecular function features from HPA (think about implications)
-    #  [X] Add cellular target localization features from HPA
-    #  [X] Check normalization after introduction of new features
-    #  [X] Hold-out test set (ACMG genes)
-    #  [X] Slot features into the feature types for spider plots
-    #  [X] Set up cross validation
-    #  [X] Hold out golden standard 32 drug targets
-    #  [X] Check overlap between golden standard and ACMG genes
-    #  [X] Check hold out drug targets for common essential genes
-    #  [X] Train baseline neural network model
-    #  [FAILED] Setup PUUPL (To unstable to use, need a properly trained model to do this. --> Self-distillation)
-    #  [X] Hyperparameter tuning run where we add logging of width/depth ratio and total number of parameters
-    #  [X] Setup self-distillation
-    #  [X] Self-distillation such that probas around the threshold are kept as unlabeled data and in this way
-    #      iteratively gather pseudolabels
-    #  [X] Train final models
     #  [ ] Evaluate on held out test set
+    #  [ ] Compare performance with missense variant pathogenicity autoencoding and without autoencoding
     #  [ ] Add SHAP interpretability
     #  -
     #  XGBoost baseline:
