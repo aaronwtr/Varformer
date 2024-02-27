64493be75c3f7892ae1cc5cceca58c4e80a830ad
Aaron <aaronwenteler@gmail.com>
2024-02-27 00:35:54 +0000

    add vae training

---
+++ src/autoencoders/vae_training.py
@@ -0,0 +1,42 @@
+import wandb
+
+from pytorch_lightning import Trainer
+from pytorch_lightning.loggers import WandbLogger
+from torch.utils.data import DataLoader
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+import src.dataloader as dl
+from src.autoencoders.vae import VAETrainer
+from src.utils import padding
+
+
+def train(data_dict, config):
+    hparams = config['hyperparameters']['pathogenicity_vae']  # Adjust if needed for VAE config
+
+    wandb.init(project='danio-autoencoders')
+
+    variant_pathogenicity = DataLoader(
+        dl.VariantPathogenicityData(data_dict=data_dict, reduct_dim=hparams['input_dim'],
+                                    reduction_type=hparams['reduction']),
+        batch_size=hparams['batch_size'],
+        collate_fn=padding,
+        shuffle=True
+    )
+
+    model = VAETrainer(input_dim=hparams['input_dim'], latent_dim=hparams['latent_dim'])  # Use your VAE model
+
+    wandb_logger = WandbLogger()
+
+    run_name = wandb_logger.experiment.name
+    checkpoint_callback = ModelCheckpoint(
+        monitor='epoch',  # May consider 'val_loss' for VAEs
+        dirpath='autoencoders/checkpoints/variant_pathogenicity_vae',  # Adjust checkpoint path
+        filename=f'{run_name}' + '-{epoch:02d}-{val_loss:.2f}',
+        save_top_k=1,
+        mode='min',
+    )
+
+    trainer = Trainer(max_epochs=hparams['max_epochs'], logger=wandb_logger, callbacks=[checkpoint_callback])
+    trainer.fit(model, variant_pathogenicity)
+
+
