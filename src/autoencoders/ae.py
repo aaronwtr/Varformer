98ced246186938abb59ff43a64badc62edf57dc5
Aaron <aaronwenteler@gmail.com>
2024-02-27 00:35:34 +0000

    cleanups

--- src/autoencoders/ae.py
+++ src/autoencoders/ae.py
@@ -1,7 +1,6 @@
 import torch
 from torch import nn
 from torch.nn import TransformerEncoder, TransformerEncoderLayer, TransformerDecoder, TransformerDecoderLayer
-import torch.nn.functional as F
 import pytorch_lightning as pl
 import math
 
