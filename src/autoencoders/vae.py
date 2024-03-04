307d65cff3a52f0df96ad02603bab2f4af810553
Aaron <aaronwenteler@gmail.com>
2024-03-04 21:08:34 +0000

    change to bce loss in vae

--- src/autoencoders/vae.py
+++ src/autoencoders/vae.py
@@ -34,7 +34,9 @@ class VAE(nn.Module):
     @staticmethod
     def loss_function(recon_x, x, mu, logvar):
         # Use MSE loss for reconstruction
-        recon_loss = nn.functional.mse_loss(recon_x, x, reduction='sum')
+        # recon_loss = nn.functional.mse_loss(recon_x, x, reduction='sum')
+        # Use BCE loss for reconstruction
+        recon_loss = nn.functional.binary_cross_entropy(recon_x, x, reduction='sum')
 
         # KL divergence
         kl_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
