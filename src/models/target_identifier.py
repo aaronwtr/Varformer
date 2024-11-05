77f199a5f7bfe354a7d3212fcb518f44fd8e922b
Aaron <aaronwenteler@gmail.com>
2024-11-05 20:29:38 +0000

    feed data into multimodal model

--- src/models/target_identifier.py
+++ src/models/target_identifier.py
@@ -90,9 +90,9 @@ class VarformerTargetIdentifier(BaseTargetIdentifier):
 
 
 class MultiModalTargetIdentifier(BaseTargetIdentifier):
-    def __init__(self, config, num_features_gc, num_features_go, num_features_pvc, num_mutations, max_seq_len,
+    def __init__(self, config, num_features_gc, num_features_go, num_mutations, max_seq_len,
                  num_genes):
-        super(MultiModalTargetIdentifier, self).__init__(config, num_features_gc + num_features_go + num_features_pvc)
+        super(MultiModalTargetIdentifier, self).__init__(config, num_features_gc + num_features_go + max_seq_len)
 
         self.gc_branch = BaseTargetIdentifier(
             config=config,
@@ -108,7 +108,7 @@ class MultiModalTargetIdentifier(BaseTargetIdentifier):
 
         self.pvc_branch = VarformerTargetIdentifier(
             config=config,
-            num_features=num_features_pvc,
+            num_features=num_features_gc,
             num_mutations=num_mutations,
             max_seq_len=max_seq_len,
             num_genes=num_genes,
@@ -150,38 +150,3 @@ class MultiModalTargetIdentifier(BaseTargetIdentifier):
         binary_predictions = (probabilities > float(self.config['threshold'])).float()
 
         return logits, probabilities, binary_predictions
-
-    def initialise_weights(self, seed=None):
-        if seed is not None:
-            torch.manual_seed(seed)
-
-        initial_weights = {}
-
-        gc_weights = self.gc_branch.initialise_weights(seed)
-        initial_weights.update({'gc_branch.' + k: v for k, v in gc_weights.items()})
-
-        go_weights = self.go_branch.initialise_weights(seed)
-        initial_weights.update({'go_branch.' + k: v for k, v in go_weights.items()})
-
-        pvc_weights = self.pvc_branch.initialise_weights(seed)
-        initial_weights.update({'pvc_branch.' + k: v for k, v in pvc_weights.items()})
-
-        for name, module in self.classification_branch.named_modules():
-            if isinstance(module, torch.nn.Linear):
-                torch.nn.init.xavier_uniform_(module.weight)
-                initial_weights[f'classification_branch.{name}.weight'] = module.weight.detach().clone()
-                if module.bias is not None:
-                    torch.nn.init.zeros_(module.bias)
-                    initial_weights[f'classification_branch.{name}.bias'] = module.bias.detach().clone()
-            elif isinstance(module, torch.nn.BatchNorm1d):
-                torch.nn.init.constant_(module.weight, 1)
-                initial_weights[f'classification_branch.{name}.weight'] = module.weight.detach().clone()
-                if module.bias is not None:
-                    torch.nn.init.constant_(module.bias, 0)
-                    initial_weights[f'classification_branch.{name}.bias'] = module.bias.detach().clone()
-                module.running_mean.fill_(0)
-                module.running_var.fill_(1)
-                initial_weights[f'classification_branch.{name}.running_mean'] = module.running_mean.detach().clone()
-                initial_weights[f'classification_branch.{name}.running_var'] = module.running_var.detach().clone()
-
-        return initial_weights
