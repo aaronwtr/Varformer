e1b5924ff72606709a2b3a34996716ac4210ecd7
Aaron <aaronwenteler@gmail.com>
2024-11-10 00:32:35 +0000

    develop multimodaldata class to handle multiple modalities of data

--- src/dataloader.py
+++ src/dataloader.py
@@ -229,124 +229,75 @@ class VarformerDataset(Dataset):
         return features
 
 
-class DrugTargetVAEData(Dataset):
-    def __init__(self, drug_target_data, reduct_dim, reduction_type="padding"):
-        # Drug Target Data
-        self.features = torch.tensor(drug_target_data['data'], dtype=torch.float32)
-        self.labels = torch.tensor(drug_target_data['labels'],
-                                   dtype=torch.float32) if 'labels' in drug_target_data else None
-        self.gene_names = drug_target_data['gene_names']
-        self.test_source = drug_target_data.get('test_source', False)
+class MultiModalData(Dataset):
+    def __init__(self, data, labels, gene_names, variant_data=None, max_variants=None, test_source=False):
+        self.data = data
+        self.labels = labels
+        self.gene_names = gene_names
+        self.variant_data = variant_data
+        self.max_variants = max_variants
+        self.test_source = test_source
 
-        # Variant Pathogenicity Data
-        # self.variant_pathogenicities = [torch.tensor(v, dtype=torch.float32) for v in drug_target_data['data']]
-        self.reduction_type = reduction_type
-        self.reduct_dim = reduct_dim
+        if self.data is not None:
+            self.features = torch.tensor(self.data, dtype=torch.float32)
+            self.labels = torch.tensor(self.labels, dtype=torch.float32)
+
+        if self.variant_data is not None:
+            self.variant_features = self.variant_data['data']
+            self.variant_labels = self.variant_data['labels']
+            self.variant_gene_names = list(self.variant_features.keys())
 
     def __len__(self):
-        return len(self.features)
-
-    def __getitem__(self, index):
-        label = self.labels[index] if self.labels is not None else torch.tensor(0.0)
-        variant_data = self.features[index]
-        variant_features = self.reduction(variant_data)
-
-        if self.test_source is False:
-            return variant_features, label
-        else:
-            return variant_features, label, self.test_source
-
-    def reduction(self, x):
-        if self.reduction_type == "padding":
-            x = self.padding(x)
-        elif self.reduction_type == "pooling":
-            x = self.pooling(x)
-        elif self.reduction_type == "None":
-            return x
-        else:
-            raise ValueError("Invalid reduction type. Expected 'padding', 'pooling' or 'None'.")
-        return x
-
-    def padding(self, x):
-        current_dimension = x.size(-1)
-        if current_dimension == self.reduct_dim:
-            return x
-        elif current_dimension < self.reduct_dim:
-            padding_left = (self.reduct_dim - current_dimension) // 2
-            padding_right = self.reduct_dim - current_dimension - padding_left
-            padded_x = F.pad(x, (padding_left, padding_right), value=0)
-            return padded_x
-        else:
-            raise ValueError("Current dimension is already greater than the target dimension.")
-
-    def pooling(self, x):
-        current_dimension = x.size(-1)
-        if current_dimension == self.reduct_dim:
-            return x
-        elif current_dimension > self.reduct_dim:
-            pool = nn.AdaptiveAvgPool1d(self.reduct_dim)
-            pooled_x = pool(x)
-            return pooled_x
-        else:
-            raise ValueError("Current dimension is already smaller than the target dimension.")
-
-    def label_imbalance(self):
-        if self.labels is not None:
-            return self.labels.sum() / len(self.labels)
+        if self.data is not None:
+            return len(self.labels)
+        elif self.variant_data is not None:
+            return len(self.variant_features)
         else:
             return 0
 
+    def __getitem__(self, index):
+        if self.data is not None:
+            if self.test_source is False:
+                return self.features[index], self.labels[index]
+            else:
+                return self.features[index], self.labels[index], self.test_source
+        elif self.variant_data is not None:
+            gene_name = self.variant_gene_names[index]
+            variants_for_gene = self.variant_features[gene_name]
+            gene_label = self.variant_labels[gene_name]
 
-class VariantPathogenicityData(Dataset):
-    def __init__(self, data_dict, reduct_dim, reduction_type="padding"):
-        self.gene_names = list(data_dict.keys())
-        self.variant_pathogenicities = list(data_dict.values())
+            pat_feat = self.padding(variants_for_gene[:, 0])
+            position = self.padding(variants_for_gene[:, 1])
+            mut_feat = self.padding(variants_for_gene[:, 2])
+            gene = self.padding(variants_for_gene[:, 3])
 
-        self.variant_pathogenicities = [torch.tensor(v, dtype=torch.float32) for v in self.variant_pathogenicities]
+            # Create attention mask
+            mask = torch.zeros(self.max_variants, dtype=torch.bool)
+            mask[len(variants_for_gene):] = 1.0
 
-        self.reduction_type = reduction_type
-        self.reduct_dim = reduct_dim
+            return {
+                'pathogenicity': pat_feat.float(),
+                'position': position.int(),
+                'mutation': mut_feat.int(),
+                'gene': gene.int(),
+                'mask': mask.float(),
+                'labels': gene_label,
+                'test_source': self.test_source
+            }
 
-    def __len__(self):
-        return len(self.gene_names)
-
-    def __getitem__(self, idx):
-        variant_pathogenicities = self.variant_pathogenicities[idx]
-        reduct_dim = self.reduct_dim
-        return variant_pathogenicities, reduct_dim
-
-    def reduction(self, x):
-        if self.reduction_type == "padding":
-            x = self.padding(x)
-        elif self.reduction_type == "pooling":
-            x = self.pooling(x)
+    def label_imbalance(self):
+        if self.data is not None:
+            return self.labels.sum() / len(self.labels)
+        elif self.variant_data is not None:
+            label_list = list(self.variant_labels.values())
+            return sum(label_list) / len(label_list)
         else:
-            raise ValueError("Invalid reduction type. Expected 'padding' or 'pooling'.")
-        return x
+            return 0
 
-    def padding(self, x):
-        current_dimension = x.size(-1)
-        if current_dimension == self.reduct_dim:
-            return x
-        elif current_dimension < self.reduct_dim:
-            # Calculate the required padding on both sides
-            padding_left = (self.reduct_dim - current_dimension) // 2
-            padding_right = self.reduct_dim - current_dimension - padding_left
-
-            # Pad the tensor
-            padded_x = F.pad(x, (padding_left, padding_right), value=0)
-            return padded_x
-        else:
-            raise ValueError("Current dimension is already greater than the target dimension.")
-
-    def pooling(self, x):
-        current_dimension = x.size(-1)
-
-        if current_dimension == self.reduct_dim:
-            return x
-        elif current_dimension > self.reduct_dim:
-            pool = nn.AdaptiveAvgPool1d(self.reduct_dim)
-            pooled_x = pool(x)
-            return pooled_x
-        else:
-            raise ValueError("Current dimension is already smaller than the target dimension.")
+    def padding(self, features):
+        if features.size(0) < self.max_variants:
+            padding = torch.zeros((self.max_variants - features.size(0)), dtype=features.dtype)
+            features = torch.cat([features, padding], dim=0)
+        elif features.size(0) > self.max_variants:
+            features = features[:self.max_variants]
+        return features
