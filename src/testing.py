d5508430298f1b4093b1dc830760dcec8a5b6b3e
Aaron <aaronwenteler@gmail.com>
2025-02-11 23:03:33 +0000

    add testing module

--- src/testing.py
+++ src/testing.py
@@ -1,49 +1,40 @@
 import torch
 import yaml
-
 from pytorch_lightning import Trainer
-from torch.utils.data import TensorDataset, DataLoader
+from torch.utils.data import DataLoader
 from preprocessing import GeneCharacterisationPreprocessor
+from models.target_identifier import MultiModalTargetIdentifier
+from dataloader import ModuleDataProcessor
 
 
-def load_model(model_path):
-    model = torch.load(model_path)
-    model.eval()
-    return model
+def load_test_data(go, gc, pvc, psc):
+    data = ModuleDataProcessor(gc, go, pvc, psc).process()
+    return data['test_data']
 
 
-def load_test_data(config):
-    gcp = GeneCharacterisationPreprocessor(config=config)
-    acmg_data = gcp.acmg_genes
-    pfam_data = gcp.pfam_genes
-    return acmg_data, pfam_data
+def test_model(test_data, model_checkpoint_path, config):
+    # TODO: Figure out how to get the feature dimensions here
+    model_init = MultiModalTargetIdentifier(
+        config=config,
+        num_features_gc=gc_features_dim,
+        num_features_go=go_features_dim,
+        num_mutations=num_mutations,
+        max_seq_len=hyperparams['max_seq_len'],
+        num_genes=max_genes_pvc
+    )
+    model = model_init.load_from_checkpoint(model_checkpoint_path, config=config)
+    test_loader = DataLoader(test_data, batch_size=config['batch_size'], shuffle=False)
+    trainer = Trainer(accelerator='gpu' if torch.cuda.is_available() else 'cpu')
+    test_results = trainer.test(model, test_loader)
 
 
-def create_dataloader(acmg_data, pfam_data, batch_size=64):
-    acmg_dataset = TensorDataset(torch.tensor(acmg_data, dtype=torch.float32))
-    acmg_dataloader = DataLoader(acmg_dataset, batch_size=batch_size)
-    pfam_dataset = TensorDataset(torch.tensor(pfam_data, dtype=torch.float32))
-    pfam_dataloader = DataLoader(pfam_dataset, batch_size=batch_size)
-    return acmg_dataloader, pfam_dataloader
-
-
-def test_model_on_acmg_data(model, acmg_dataloader, pfam_dataloader):
-    trainer = Trainer()
-
-    # Testing on ACMG data
-    trainer.test(model, dataloaders=acmg_dataloader)
-
-    # Testing on pfam data
-    trainer.test(model, dataloaders=pfam_dataloader)
-
-
-def test():
-    with open("config.yml", 'r') as stream:
-        config = yaml.safe_load(stream)
-    model_path = 'path_to_your_model.pt'  # Replace with your model path
-
-    model = load_model(model_path)
-    acmg_data, pfam_data = load_test_data(config)
-    acmg_dataloader, pfam_dataloader = create_dataloader(acmg_data, pfam_data)
-
-    test_model_on_acmg_data(model, acmg_dataloader, pfam_dataloader)
+def run_test(**modules):
+    gc = modules.get('gc', False)
+    go = modules.get('go', False)
+    pvc = modules.get('pvc', False)
+    psc = modules.get('psc', False)
+    test_data = load_test_data(gc, go, pvc, psc)
+    model_ckpt_path = 'checkpoints/seed42-epochepoch=76-val_aurocval_auroc=0.90.ckpt'
+    with open('config.yml', 'r') as file:
+        config = yaml.safe_load(file)
+    test_model(test_data, model_ckpt_path, config)
