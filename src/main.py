555e14dfb8c7f48824c8d09bbf3dd9076c4ba6b4
Aaron <aaronwenteler@gmail.com>
2025-05-27 21:19:21 +0100

    small refactoring main function

--- src/main.py
+++ src/main.py
@@ -6,30 +6,24 @@ from inference import run_inference_pipeline
 
 def main(mode="training", config=None, checkpoint=None, output=None):
     if mode == "training":
-        training.train(tag="Standard Training")
-    elif mode == "puupl":
-        training.train(tag="PUUPL Training")
+        training.setup_training(pvc=True, go=True, gc=True, config=config)
     elif mode == "tuning":
         training.tune()
-    elif mode == "kfold_teacher":
-        training.kfold_teacher(pvc=True, go=True, gc=True, config=config)
-    elif mode == "kfold_student":
-        training.kfold_student()
-    elif mode == "logistic_regression":
-        training.logistic_regression(pvc=True, go=True, gc=True, config=config)
     elif mode == "testing":
         testing.run_test(pvc=True, go=True, gc=True)
-    elif mode == "random":
-        training.random(pvc=True, go=True, gc=True, config=config)
-    elif mode == "drugnome_ai":
-        training.drugnome_ai(pvc=True, go=True, gc=True, config=config)
     elif mode == "inference":
         if not checkpoint or not output:
             raise ValueError("For inference mode, both --checkpoint and --output arguments are required.")
         run_inference_pipeline(checkpoint=checkpoint, output=output)
+    elif mode == "logistic_regression":
+        training.logistic_regression(pvc=True, go=True, gc=True, config=config)
+    elif mode == "random":
+        training.random(pvc=True, go=True, gc=True, config=config)
+    elif mode == "drugnome_ai":
+        training.drugnome_ai(pvc=True, go=True, gc=True, config=config)
     else:
-        raise ValueError("Invalid mode. Pick from 'training', 'tuning', 'kfold_student', 'kfold_teacher', "
-                         "'testing', 'puupl', 'inference', or others.")
+        raise ValueError("Invalid mode. Pick from 'training', 'tuning', 'testing', 'inference', 'random',"
+                         "logistic_regression, 'drugnome_ai or others.")
 
 
 if __name__ == "__main__":
